edd113b2773317bcf4d739f453714d2f66641935
pe-chan816 <bouncing.yamapie816@gmail.com>
2021-12-25 23:05:17 +0900

    TaskControllerにFolderとの関連を追加

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -3,17 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Folder;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
   public function index(int $id)
   {
+    // 全ての　Folder を取得する
     $folders = Folder::all();
+
+    // 選ばれた Folder を取得する
+    $current_folder = Folder::find($id);
+
+    // 選ばれた Folder に紐づく Task を取得する
+    $tasks = Task::where('folder_id', $current_folder->id)->get();
 
     return view('tasks/index',[
       'folders' => $folders,
-      'current_folder_id' => $id
+      'current_folder_id' => $current_folder->id,
+      'tasks' => $tasks
     ]);
   }
 }
