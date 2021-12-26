e113c163416445f34ae3dd33060508fba5000d86
pe-chan816 <bouncing.yamapie816@gmail.com>
2021-12-27 07:37:21 +0900

    viewをテンプレートで書き直し

--- backend/resources/views/tasks/index.blade.php
+++ backend/resources/views/tasks/index.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE HTML>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edje">
-  <title>ToDo App</title>
-  <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-<header>
-  <nav class="my-navbar">
-    <a class="my-navbar-brand" href="/">ToDo App</a>
-  </nav>
-</header>
-<main>
+@extends('html')
+
+@section('main')
   <div class="container">
     <div class="row">
 
@@ -75,6 +62,4 @@
 
     </div>
   </div>
-</main>
-</body>
-</html>
+@endsection
