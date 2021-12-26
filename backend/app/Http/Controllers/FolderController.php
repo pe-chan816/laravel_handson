c23da1bd2e985a270093fd0e4d38a245d4f485f5
pe-chan816 <bouncing.yamapie816@gmail.com>
2021-12-26 14:37:44 +0900

    入力バリデーションの追加

--- backend/app/Http/Controllers/FolderController.php
+++ backend/app/Http/Controllers/FolderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Folder;
+use App\Http\Requests\CreateFolder;
 
 class FolderController extends Controller
 {
@@ -12,7 +13,7 @@ class FolderController extends Controller
     return view('folders/create');
   }
 
-  public function create(Request $request)
+  public function create(CreateFolder $request)
   {
     //Folderモデルのインスタンスを作成
     $folder = new Folder();
--- backend/resources/views/folders/create.blade.php
+++ backend/resources/views/folders/create.blade.php
@@ -20,11 +20,20 @@
         <nav class="panel panel-default">
           <div class="panel-heading">フォルダを追加する</div>
           <div class="panel-body">
+            @if($errors->any())
+              <div class="alert alert-danger">
+                <ul>
+                  @foreach($errors->all() as $message)
+                    <li>{{ $message }}</li>
+                  @endforeach
+                </ul>
+              </div>
+            @endif
             <form action="{{ route('folders.create') }}" method="POST">
               @csrf
               <div class="form-group">
                 <label for="title">フォルダ名</label>
-                <input type="text" name="title" class="form-control" id="title">
+                <input type="text" name="title" class="form-control" id="title" value="{{ old('title') }}">
               </div>
               <div class="text-right">
                 <button type="submit" class="btn btn-primay">送信</button>
