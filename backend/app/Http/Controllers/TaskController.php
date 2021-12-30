28e1ecb91493957a2fe60ebf19820b86c796b379
pe-chan816 <bouncing.yamapie816@gmail.com>
2021-12-30 14:49:38 +0900

    Task編集機能のバリデーション追加

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Folder;
 use App\Models\Task;
 use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -55,6 +56,20 @@ class TaskController extends Controller
 
     return view('tasks/edit', [
       'task' => $task
+    ]);
+  }
+
+  public function edit(int $id, int $task_id, EditTask $request)
+  {
+    $task = Task::find($task_id);
+
+    $task->title = $request->title;
+    $task->status = $request->status;
+    $task->due_date = $request->due_date;
+    $task->save();
+
+    return redirect()->route('tasks.index', [
+      'id' => $task->folder_id
     ]);
   }
 }
